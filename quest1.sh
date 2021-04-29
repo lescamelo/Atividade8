11bc687ef7ca6f11ff6f1b131dc26077d0287e6d
Valesca Melo <valesca.melo@academico.ifpb.edu.br>
2021-04-29 10:55:32 -0300

    1

--- quest1.sh
+++ quest1.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-arquivo=${1}
-numero=${2}
+arquivo=$1
+numero=$3
 
 if [ -f ${arquivo} ];
 then
-	count=$(wc -l $arquivo | cut -c 1)
+	count=$(wc -l ${arquivo}  |  cut -c 1)
 	if [ ${count} -lt ${numero} ];
 	then
-		echo "Falha: O arquivo ${arquivo} possui ${count} linhas!!"
+		echo "Falha: ${arquivo} possui ${count} linhas"
 	else
 		cat ${arquivo} | head -${numero} | tail -1
 	fi
 else
-	echo "ERRO: O  ARQUIVO NÃO EXISTE."
+	echo "Erro: O ARQUIVO NÃO EXISTE."
 
 fi
+
+
 
 
